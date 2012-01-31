e1496193cb25ee40c9823cb14cbb85130bcdb106
Xyntrix <xyntrix@gmail.com>
2012-01-30 18:20:55 -0700

    o add alternative ruby install paths to install script o add verbosity to install.sh script, so we know where it copied the file to o removed space from interpreter

--- install.sh
+++ install.sh
@@ -1,10 +1,19 @@
-#! /bin/sh
+#!/bin/sh
 
 DIR=$(dirname $0)
 YLOOKUP=ylookup.rb
+
+RUBYPREFIX="/usr/lib"
+[ -d "/usr/lib64/ruby/site_ruby/1.8/" ] && RUBYPREFIX="/usr/lib64"
+[ -d "/opt/lib/ruby/site_ruby/1.8/" ] && RUBYPREFIX="/opt/lib"
+[ -d "/opt/lib64/ruby/site_ruby/1.8/" ] && RUBYPREFIX="/opt/lib64"
+
+RUBYSUFFIX="/ruby/site_ruby/1.8/puppet/parser/functions/"
+RUBYPATH=${RUBYPREFIX}${RUBYSUFFIX}
+
 if [ -x /usr/bin/install ] ; then
-    install --mode=0644 --owner=root --group=root $DIR/$YLOOKUP /usr/lib/ruby/site_ruby/1.8/puppet/parser/functions/ylookup.rb
-else 
-    cp $DIR/$YLOOKUP /usr/lib/ruby/site_ruby/1.8/puppet/parser/functions/ylookup.rb
-    chown root:root /usr/lib/ruby/site_ruby/1.8/puppet/parser/functions/ylookup.rb
+    install -v --mode=0644 --owner=root --group=root $DIR/$YLOOKUP ${RUBYPATH}/ylookup.rb
+else
+    cp -v $DIR/$YLOOKUP ${RUBYPATH}/ylookup.rb
+    chown -c root:root ${RUBYPATH}/ylookup.rb
 fi
